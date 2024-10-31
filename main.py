d7f5e303005af4c3736a4f57be3de87b5a2f589e
taicy013 <taicy.tony@laetottenham.ac.uk>
2024-10-31 14:26:55 +0000

    cats

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from random import randint
+
+def play(car):
+    car['energy'][0] -= 5
+    car['energy'][1] += 3
+    pick = randint(57,3065)
+    if pick > 3000:
+        if 'hunting' in car.keys():
+            car['hunting'] += 3
+        else: 
+            car['hunting'] = 5
+            print("New skill acqired!:- 'HUNTING'")
+    
+
+
+
+cat = {'name':'',
+        'colour': '',
+        'intelligence': 10,
+        'energy': [50,50],
+        'weight': 10
+    }
+
+cat['name'] = input("Enter cat's name: ")
+cat['colour'] = input("Enter cat's colour: ")
+
 
